e30664afb94c63e8a4837a8671e6354807a14598
rowenarono95 <rowenarono@gmail.com>
2020-11-21 17:06:59 +0300

    add profile models

--- gramclone/models.py
+++ gramclone/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=30)
+    profile_pic = models.ImageField(upload_to='new_post/', blank = 'true')
+    bio = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='', null=True)
+
