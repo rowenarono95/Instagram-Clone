595b1f21b1d0bdb09cad4ca3452d49d4b0320f95
rowenarono95 <rowenarono@gmail.com>
2020-11-21 18:40:05 +0300

    saveprofile functionality

--- gramclone/models.py
+++ gramclone/models.py
@@ -2,12 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Profile(models.Model):
     name = models.CharField(max_length=30)
-    profile_pic = models.ImageField(upload_to='new_post/', blank = 'true')
+    profile_pic = models.ImageField()
     bio = models.TextField()
     user = models.OneToOneField(User, on_delete=models.CASCADE, default='', null=True)
 
+
+    def save_profile(self):
+        self.
--- gramclone/url.py
+++ gramclone/url.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import views,include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
